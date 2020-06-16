6b4d3d6c5fd44335b0d756309acb2b9e367767be
Lemur-lab <52214472+Lemur-lab@users.noreply.github.com>
2020-06-16 11:15:49 +0200

    Ecriture de la classe CCompas

--- ROBIO/ROBIO/CCompas.h
+++ ROBIO/ROBIO/CCompas.h
@@ -1,5 +1,17 @@
 #pragma once
 class CCompas
 {
+	// Données privées
+private :
+	int position_x;
+	int position_y;
+
+	// Données publiques
+public :
+	int get_position_x()				{ return position_x; }
+	int get_position_y()				{ return position_y; }
+
+	void set_position_x(int pos_x)		{ position_x = pos_x; }
+	void set_position_y(int pos_y)		{ position_y = pos_y; }
 };
 
